6f33775ff2ef31a4a21a8b4bf9291dde27d1f5f0
Craig McDaniel <craig.mcdaniel@db.com>
2014-03-28 17:25:56 -0400

    Improved ease of execution and documentation.

--- yaml-migration/README.md
+++ yaml-migration/README.md
@@ -1,14 +1,46 @@
 # yaml-migration
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure utility designed to allow migration from one YAML config file version to a newer version. The newer version may have new options and may have removed old ones which are now obsolete. This implementation has been made with the following assumptions and limitations:
+
+1. The new file is assumed to be a sample contain all valid options with reasonable default values for the latest version of the configuration.
+2. If an option exists in both files, the old value will be kept in the output.
+3. If an option has been removed, it will be commented out at the bottom of the output.
+4. If a new option has been added, it will be included in the output with its default value from the new file.
+5. Comments, ordering, and other formatting is not currently preserved.
+6. Does not currently handle deeply-nested changes (untested!).
 
 ## Usage
 
-FIXME
+There are a few ways to execute this code.
+
+### At the REPL
+
+You can use _lein repl_ in the root of the project to run the code interactively. The file paths are relative to the project root.
+
+```clojure
+(use 'yaml-migration.migrate)
+(migrate-yaml "resources/file1.yaml" "resources/file2.yaml" "target/out.yaml")
+```
+
+### From the command line with lein
+
+Alternatively, you can execute it from the command line with _lein run_.
+
+```
+$ lein run -m yaml-migration.migrate resources/file1.yaml resources/file2.yaml target/out.yaml
+```
+
+### From the command line with a JAR
+
+Finally, you could package everything up using _lein uberjar_ and run it like so:
+
+```
+$ java -jar target/yaml-migration-0.1.0-SNAPSHOT-standalone.jar \
+resources/file1.yaml resources/file2.yaml target/out.yaml
+```
 
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2014 Craig McDaniel
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Distributed under the Eclipse Public License version 1.0.
--- yaml-migration/src/yaml_migration/migrate.clj
+++ yaml-migration/src/yaml_migration/migrate.clj
@@ -2,32 +2,41 @@
   (:require [clj-yaml.core :as yaml]
             [clojure.java.io :as io]
             [clojure.data :as d]
-            [clojure.string :refer [join]])
-  (:import [java.io StringReader]))
+            [clojure.string :as st :refer [join]])
+  (:import [java.io StringReader]
+           [java.util.regex Pattern])
+  (:gen-class :main true))
 
+(defn comment-lines
+  "Prepends each line with '# '"
+  [^String s]
+  (st/replace s (Pattern/compile "^" Pattern/MULTILINE) "# "))
 
-(defn comment-lines [^String s]
-  (with-open [r (io/reader (StringReader. s))]
-    (doall
-     (join "\n"
-      (map #(str "#" %) (line-seq r))))))
+(defn diff-to-str
+  "Takes old-only data, new-only data, and data in both, and generates the YAML
+   string."
+  [old-only new-only both]
+  (str (yaml/generate-string both)
+       "\n\n## New defaults in this version of the config:\n"
+       (yaml/generate-string new-only)
+       "\n\n## The following options are no longer valid in this version of the config:\n"
+       (comment-lines (yaml/generate-string old-only))))
 
-(let [[a-only b-only both]
-      (d/diff
-       (yaml/parse-string (slurp "resources/file1.yaml"))
-       (yaml/parse-string (slurp "resources/file2.yaml")))]
-
-
-  (spit "target/out.yaml"
-        (str (yaml/generate-string both)
-             "\n\n## New defaults in this version of Cassandra:\n"
-             (yaml/generate-string b-only)
-             "\n\n## The following options are no longer valid in this version of Cassandra:\n"
-             (comment-lines (yaml/generate-string a-only)))))
-
-
-
-(defn -main [[old-file new-file]]
+(defn migrate-yaml
+  "Takes an old file and a new file, and attempts to migrate to the
+   new file version."
+  [old-file new-file out-file]
+  ;; Uses clojure.data/diff to get the differences between the 2
+  ;; data structures.
   (let [old-data (yaml/parse-string (slurp old-file))
         new-data (yaml/parse-string (slurp new-file))]
-    ))
+    (spit out-file
+          (apply diff-to-str (d/diff old-data new-data)))))
+
+;; Allow command line usage
+(defn -main [& args]
+  (apply migrate-yaml args))
+
+(comment
+  ;; For testing at the REPL
+ (migrate-yaml "resources/file1.yaml" "resources/file2.yaml" "target/out.yaml"))
